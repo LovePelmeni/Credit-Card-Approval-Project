db23894ba94ed66811dc1f7422298b7420b3710b
= <kirklimushin@gmail.com>
2023-06-28 20:08:36 +0300

    Applied Sampling Technique (SMOTE) for project's dataset
    What changed:
    1. Data was imbalanced and the number of bad clients encompassed around 7% of the entire dataset
    2. Fixed dataset imbalance using SMOTE Sampling Technique
    
    NOTES:
    1. Assume, SMOTE might not be the most appropriate technique in this case, gonna conduct several experiments in the future
    to make sure that sampling strategy was selected correctly

--- src/cleansing/clean.py
+++ src/cleansing/clean.py
@@ -1,8 +1,20 @@
 import numpy
 import pandas
+import logging
+import sampling
 
 applications = pandas.read_csv("application_record.csv")
 credit_dataset = pandas.read_csv("credit_record.csv")
 
-print(applications.info())
-print(credit_dataset.info())
+Logger = logging.getLogger(__name__)
+
+# Cleansing data and engineering features 
+
+
+# Handling Imbalanced dataset 
+
+
+# Feature Selection 
+
+
+# Dimensionality Reduction 
