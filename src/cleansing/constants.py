0b24c8701ef07d12d6517371a58ce354fb860c9e
= <kirklimushin@gmail.com>
2023-06-29 02:20:21 +0300

    Added Feature Encoding for binary variables
    What changed:
    1. Field, that contained binary values (YES/NO) now are imputed to corresponding boolean analogies (True / False)
    2. Imputed Missing values in the entire dataset

--- src/cleansing/constants.py
+++ src/cleansing/constants.py
@@ -1,4 +1,5 @@
 """
 File for storing constant values for different files throughout the project
 """
-K_SMOTE_NEIGHBORS = 4
+K_SMOTE_NEIGHBORS = 4 # number of neighbors for smote sampling technique
+MISSING_JOB = "missing_job" # Missing Job Category for null values in 'Job' feature
