c527ab04d04489485371b05d2177f95f2cbefa14
= <kirklimushin@gmail.com>
2023-07-08 02:43:49 +0300

    removed extra files

--- src/cleansing/clean.py
+++
@@ -1,8 +0,0 @@
-import numpy
-import pandas
-
-applications = pandas.read_csv("application_record.csv")
-credit_dataset = pandas.read_csv("credit_record.csv")
-
-print(applications.info())
-print(credit_dataset.info())
