915c73ef055ba04e07c367f353e0bc35cf565e13
= <kirklimushin@gmail.com>
2023-07-12 12:38:46 +0300

    Deployment Pipeline Completion

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ fi
 
 echo "Starting ASGI Server..."
 
-python ./settings.py
+uvicorn settings:application --host 0.0.0.0 --port 8080
 
 if [ $? -ne 0 ]; then
     echo "ASGI Server Startup Failure"
