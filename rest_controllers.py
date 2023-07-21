5184d2091587ba7d4fc3681caab8eecc850cb372
= <kirklimushin@gmail.com>
2023-07-21 11:15:56 +0300

    Fixed Codestyle

--- rest_controllers.py
+++ rest_controllers.py
@@ -6,6 +6,7 @@ import fastapi.requests
 
 logger = logging.getLogger(__name__)
 
+
 def predict_card_approval(application_data: feature_form.CardApprovalFeatures):
     """
     Function predicts whether client would be allowed to have a credit card or not 
@@ -13,22 +14,24 @@ def predict_card_approval(application_data: feature_form.CardApprovalFeatures):
         application_data: Feature Dataset with client's personal informaton
     """
     try:
-        predicted_status = modeling.prediction_model.predict_approval(application_data)
+        predicted_status = modeling.prediction_model.predict_approval(
+            application_data)
         return fastapi.responses.JSONResponse(
             status_code=201,
             content={
-                "status": "Approved" if predicted_status == 1 else "Rejected",
+                "status": predicted_status
             }
         )
-    except(ValueError) as val_err:
-        return fastapi.exceptions.HTTPException(
-        status_code=400, detail=val_err.args)
 
-    except(exceptions.PredictionFailed) as prediction_err:
+    except (ValueError) as val_err:
+        return fastapi.exceptions.HTTPException(
+            status_code=400, detail=val_err.args)
+
+    except (exceptions.PredictionFailed) as prediction_err:
         logger.error("""Failed to predict application
         allowance for credit card approval. [%s]""" % prediction_err)
-        return fastapi.responses.Response(status_code=500, 
-        content={'error': 'server failed to predict status, internal error :('})
+        return fastapi.responses.Response(status_code=500,
+                                          content={'error': 'server failed to predict status, internal error :('})
 
 
 def healthcheck(request: fastapi.requests.Request):
