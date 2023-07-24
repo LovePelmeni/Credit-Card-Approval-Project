2ba3fee11475751afe998580ee6101b750785f47
= <kirklimushin@gmail.com>
2023-07-24 11:54:11 +0300

    Removed Extra Folders

--- src/modeling/constants.py
+++ src/modeling/constants.py
@@ -1,2 +1,4 @@
 DECISION_THRESHOLD = 0.9783825085241585
 MODEL_CLASSIFIER_URL="src/offline_training/classifier.pkl"
+FEATURE_ORDER  =  ['Male', 'has_car', 'Married', 'Female', 'has_realty',
+       'credit_window', 'annual_income', 'total_children']
