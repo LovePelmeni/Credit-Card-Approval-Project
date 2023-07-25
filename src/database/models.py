07ef5760bd195b58844f1942fee2e1ccee5d63f1
= <kirklimushin@gmail.com>
2023-07-25 16:58:45 +0300

    Renamed Tables

--- src/database/models.py
+++ src/database/models.py
@@ -4,17 +4,17 @@ from sqlalchemy.orm import declarative_base
 Model = declarative_base()
 class CreditTransaction(Model):
 
-    __tablename__ = "CreditTransactions"
+    __tablename__ = "transactions"
 
-    ID = Column(Integer, unique=True, primary_key=True, index=True, nullable=False)
+    transaction_id = Column(Integer, unique=True, primary_key=True, index=True, nullable=False)
     customer_id = Column(Integer, unique=True, primary_key=True)
     bad = Column(Boolean, default=False)
 
 class CustomerApplication(Model):
 
-    __tablename__ = "CustomerApplications"
+    __tablename__ = "applications"
 
-    ID = Column(Integer, unique=True, primary_key=True, index=True, nullable=False)
+    customer_id = Column(Integer, unique=True, primary_key=True, index=True, nullable=False)
     email = Column(Integer, unique=True, nullable=False)
     annual_income = Column(Float, nullable=False)
     total_children = Column(Integer, nullable=True)
